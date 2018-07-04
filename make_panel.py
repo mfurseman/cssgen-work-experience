30b98b463534dddd3994a484baeca79672c27e2e
Oscar Mcauley <qqn39519@lapt0085.cs.diamond.ac.uk>
2018-07-04 09:31:54 +0100

    Comments added to give context

--- make_panel.py
+++ make_panel.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env dls-python2.7
 
-from pkg_resources import require
-require('cssgen==0.4dls1')
+from pkg_resources import require #imports require function from pkg_resources library
+require('cssgen==0.4dls1') #runs require function with argument
 
-from opimodel import widgets, colors, fonts, dls_utils
-from renderers.css import render
+from opimodel import widgets, colors, fonts, dls_utils #imports functions from opimodel library
+from renderers.css import render #imports render function from renderer.css library
 
 
-FILENAME = 'test.opi'
+FILENAME = 'test.opi' #sets the target file in folder
 
 colors.parse_css_color_file(dls_utils.find_color_file())
 
 
-display = widgets.Display(588, 180)
-rectangle = widgets.Rectangle(0, 0, 50, 50)
-display.add_child(rectangle)
+display = widgets.Display(588, 180) #sets display size
+rectangle = widgets.Rectangle(0, 0, 50, 50) #makes rectangle variable 50x50px
+display.add_child(rectangle) #uses add_child method to add rectangle to canvas
 
 # Write out the display
 render.get_opi_renderer(display).write_to_file(FILENAME)
