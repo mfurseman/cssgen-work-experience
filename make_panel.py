3880e1df605df5f23b5ed7f3984e472b01e8e60c
Oscar Mcauley <qqn39519@lapt0085.cs.diamond.ac.uk>
2018-07-05 12:44:20 +0100

    improved cell generation

--- make_panel.py
+++ make_panel.py
@@ -3,7 +3,7 @@
 from pkg_resources import require #imports require function from pkg_resources library
 require('cssgen==0.4dls1') #runs require function with argument
 
-from opimodel import widgets, colors, fonts, dls_utils #imports functions from opimodel library
+from opimodel import widgets, colors, fonts, dls_utils, borders #imports functions from opimodel library
 from renderers.css import render #imports render function from renderer.css library
 
 numOfCells = 24
@@ -11,22 +11,32 @@ numOfCells = 24
 FILENAME = 'test.opi' #sets the target file in folder
 
 colors.parse_css_color_file(dls_utils.find_color_file())
+fonts.parse_css_font_file(dls_utils.find_font_file())
 
+display = widgets.Display(604, 367) #sets display size
+#rectangle = widgets.Rectangle(0, 0, 50, 50) #makes rectangle variable 50x50px
+#display.add_child(rectangle) #uses add_child method to add rectangle to canvas
 
-display = widgets.Display(588, 180) #sets display size
-rectangle = widgets.Rectangle(0, 0, 50, 50) #makes rectangle variable 50x50px
-display.add_child(rectangle) #uses add_child method to add rectangle to canvas
-
-def initialiseTopCells(canvasSize, xCanvasMargin, yCanvasMargin, cellSize, numOfCells):
+def initialiseTopCells(canvasWidth, xCanvasMargin, yCanvasMargin, cellSize, numOfCells):
 	xpos = xCanvasMargin
 	ypos = yCanvasMargin
 	count = 0
 
 	for cell in range(numOfCells):
 		count += 1
-		xpos += (canvasSize-(xCanvasMargin*2)-(numOfCells*cellSize))/numOfCells
-		square = widgets.Rectangle(xpos, ypos, cellSize, cellSize)
-		square.add_write_pv(cell) #REPLACE WITH ACTUAL SYNTAX
+		cell = str(cell+1)
+		xpos += (canvasWidth/numOfCells) - ((xCanvasMargin*2)/numOfCells)
+		cellLabel = widgets.Label(xpos, ypos, cellSize, cellSize, cell)
+		cellLabel.set_font(fonts.FINE_PRINT)
+		cellLabel.set_bg_color(colors.GREY_90_)
+		cellLabel.set_transparency(False)
+		cellLabel.set_border(borders.Border(borders.LINE_STYLE, 1, colors.BLACK, False))
+		display.add_child(cellLabel)
 	
+
+initialiseTopCells(604, 13, 2, 20, 24)
+
+display.set_bg_color(colors.WHITE)
+
 # Write out the display
 render.get_opi_renderer(display).write_to_file(FILENAME)
